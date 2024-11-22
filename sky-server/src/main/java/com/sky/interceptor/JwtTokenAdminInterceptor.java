df58f7fd584bee479c442a4c286dd659d2b6f599
whpz <2206373437@qq.om>
2024-11-22 19:11:26 +0800

    店铺营业状态设置业务代码

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -49,7 +49,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
             //将当前的id放入threadlocal中，方便service层获取
             BaseContext.setCurrentId(empId);
-            log.info("当前员工id：", empId);
+            log.info("当前员工id：{}",empId);
             //3、通过，放行
             return true;
         } catch (Exception ex) {
