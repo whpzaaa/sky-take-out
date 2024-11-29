aeb461aa455acfb19dce7c6588661f20a2d79566
whpz <2206373437@qq.om>
2024-11-29 15:38:19 +0800

    添加购物车相关代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+    @Update("update sky_take_out.shopping_cart set number = #{number} where id = #{id} ")
+    void updateById(ShoppingCart shoppingCart);
+    @Insert("insert into sky_take_out.shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time,number) " +
+            "VALUES (#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{amount},#{createTime},#{number})")
+    void insert(ShoppingCart shoppingCart);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,61 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Autowired
+    private SetmealMapper setmealMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    public void add(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+        //先根据dishId和setmealId查询数据库中是否存在该数据
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+        //若结果不为空 则代表有数据 则数量加一即可
+        if (shoppingCartList != null && shoppingCartList.size() > 0) {
+            shoppingCart = shoppingCartList.get(0);
+            shoppingCart.setNumber(shoppingCart.getNumber() + 1);
+            shoppingCartMapper.updateById(shoppingCart);
+        }//若结果为空 则无数据 要向表中添加数据
+        else {
+            //如果dishId不为空 则传入的是菜品 需要查询菜品表
+            Long dishId = shoppingCartDTO.getDishId();
+            if (dishId != null){
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+            }//反之则是套餐
+            else {
+                Setmeal setmeal = setmealMapper.getById(shoppingCartDTO.getSetmealId());
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+        }
+
+    }
+}
